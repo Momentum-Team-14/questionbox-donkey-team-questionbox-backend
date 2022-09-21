571f8ee2caf0723463622bf1dcb4f5f0294cef67
cbmosley <cbmosley@ncsu.edu>
2022-09-21 16:50:08 -0400

    Put together the basic models.

--- question_app/models.py
+++ question_app/models.py
@@ -3,8 +3,38 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
+
 class CustomUser(AbstractUser):
+    profile_picture = models.ImageField(
+        upload_to='users', blank=True, null=True)
+
     pass
 
     def __str__(self):
         return self.username
+
+
+class Question(models.Model):
+    user = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name="question")
+    question_field = models.TextField(max_length=10000, blank=True, default='')
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.question_field
+
+
+class Answer(models.Model):
+    user = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name="answer")
+    answer_field = models.TextField(max_length=10000, blank=True, default='')
+    date_answered = models.DateTimeField(auto_now_add=True)
+    accepted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.answer_field
+
+
+class Favorite(models.Model):
+    question = models.ForeignKey(
+        Question, on_delete=models.CASCADE, related_name="favorite")
